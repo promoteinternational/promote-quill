adcba87cfa699803d5deb2537832a9be32a87430
Jimmie Elvenmark <flugsio@gmail.com>
2020-04-21 15:41:52 +0000

    Fix borked tests

--- test/test_promote_link.js
+++ test/test_promote_link.js
@@ -9,6 +9,7 @@ describe('PromoteLink', () => {
   it('add https if protocol is missing', () => {
     let subject = PromoteLink.create("example.org");
     expect(subject.getAttribute('href')).to.equal("https://example.org");
+  });
 
   it("includes blank noopener noreferrer attributes", () => {
     let subject = PromoteLink.create("https://example.org");
