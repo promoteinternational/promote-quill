2ad91bb3bed8726e2ebd62a668c297d2c02f7bde
Michael Winton <wintonmr@gmail.com>
2020-04-21 11:23:21 +0200

    [#169176122] Update links to open in new window
    The goal of this story is to update internal links to external sites
    to use `_blank`, opening the site in a new window, with `noopener
    norefferer` to prevent hijacking of the origin tab (Promote in this
    case).
    
    This commit progresses this story by updating Quill links to have
    these attributes.

--- test/test_promote_link.js
+++ test/test_promote_link.js
@@ -9,6 +9,11 @@ describe('PromoteLink', () => {
   it('add https if protocol is missing', () => {
     let subject = PromoteLink.create("example.org");
     expect(subject.getAttribute('href')).to.equal("https://example.org");
+
+  it("includes blank noopener noreferrer attributes", () => {
+    let subject = PromoteLink.create("https://example.org");
+    expect(subject.getAttribute("target")).to.equal("_blank");
+    expect(subject.getAttribute("rel")).to.equal("noopener noreferrer");
   });
 });
 
